cdca631820781bdfc124c8893de2130497384244
funnyrussianigoryan <funnyrussianigoryan@gmail.ru>
2025-07-25 23:44:12 +0700

    delete tests

--- tests/unit/test_cli.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from aemon.cli import main
-
-
-def test_main_logs_message(caplog):
-    with caplog.at_level(logging.INFO):
-        main()
-    assert "Test message" in caplog.text
